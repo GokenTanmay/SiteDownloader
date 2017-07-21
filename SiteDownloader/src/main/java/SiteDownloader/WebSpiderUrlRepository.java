a63d6f1aaa34ac52392e2f08b3b2480ec37ad04e
GokenTanmay <gkntnm@gmail.com>
2017-07-21 14:18:04 +0400

    Repository for save SpiderURL

---
+++ SiteDownloader/src/main/java/SiteDownloader/WebSpiderUrlRepository.java
@@ -0,0 +1,45 @@
+package SiteDownloader;
+
+import java.util.HashMap;
+
+/**
+ * Created by Serg on 06.07.2017.
+ */
+public class WebSpiderUrlRepository {
+    private HashMap unscanedURL;
+    private HashMap scanedURL;
+
+    public WebSpiderUrlRepository() {
+        unscanedURL = new HashMap();
+        scanedURL = new HashMap();
+    }
+
+    /**
+     * Метод добавляет SpiderURL и контролиркет его неповторяемость.
+     *
+     * @param spiderURL - Класс описывающий URL и глубину его сслыки
+     */
+    public void addUnscanedURL(SpiderURL spiderURL) {
+        if ((!unscanedURL.containsKey(spiderURL.getUrl())) && (!scanedURL.containsKey(spiderURL.getUrl()))) {
+            unscanedURL.put(spiderURL.getUrl(), spiderURL.getLevel());
+        }
+    }
+
+    /**
+     * @return - Возвращает SpiderURL которые еще ни разу не брались для сканирования. Или если таковых нет то null;
+     */
+    public SpiderURL getUnscanedURL() {
+        SpiderURL result = null;
+        if (unscanedURL.entrySet().iterator().hasNext()) {
+            HashMap.Entry<String, Integer> firstEntry = (HashMap.Entry<String, Integer>) unscanedURL.entrySet().iterator().next();
+            result = new SpiderURL(firstEntry.getKey(), firstEntry.getValue());
+            unscanedURL.remove(firstEntry.getKey());
+            scanedURL.put(firstEntry.getKey(), firstEntry.getValue());
+        }
+        return result;
+    }
+
+    public HashMap getHashMapScanedURL() {
+        return scanedURL;
+    }
+}
