cbdd555b48a795db32946f7b26731b705dfb81a9
GokenTanmay <gkntnm@gmail.com>
2017-07-24 17:19:39 +0400

    Изменение тестового файла.

--- SiteDownloader/src/main/java/SiteDownloader/AppTest1.java
+++ SiteDownloader/src/main/java/SiteDownloader/AppTest1.java
@@ -3,6 +3,7 @@ package SiteDownloader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class AppTest1 {
     public static void main(String[] args) {
@@ -22,6 +23,17 @@ public class AppTest1 {
 
         Parser parser = new Parser(sb.toString(), "body");
 
-        System.out.println(parser.getTextOnly().replaceAll("[,.\n]", ""));
+        System.out.println(new ConverterStringToHasMap().convert(parser.getTextOnly()));
+//
+//        HashMap hashMap = new HashMap();
+//        hashMap.put("1", 1);
+//        hashMap.put("2", 2);
+//
+//        System.out.println(hashMap);
+//
+//        hashMap.put("1", 3);
+//
+//        System.out.println(hashMap);
+//        System.out.println(hashMap.get("1"));
     }
 }
--- SiteDownloader/src/main/java/SiteDownloader/ConverterStringToHasMap.java
+++ SiteDownloader/src/main/java/SiteDownloader/ConverterStringToHasMap.java
@@ -1,15 +1,25 @@
 package SiteDownloader;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ConverterStringToHasMap {
     public HashMap convert(String string){
         HashMap hashMap = new HashMap();
-        //Очищаем строку от запятых, точек и переносов.
+        //Очищаем строку от запятых, точек и переносов и прочих знаков препинаний.
         String tmpString = "";
-        tmpString.replaceAll("[,.\n]", "");
+        tmpString = string.replaceAll("[,.\n\\[\\]\"\\(\\)\\«\\»\\—]", "").toLowerCase();
+        System.out.println(tmpString.split(" ").length);
 
+        for (String word: tmpString.split(" ")) {
+            if(hashMap.containsKey(word)){
+                Integer i = (Integer) hashMap.get(word);
+                i++;
+                hashMap.put(word, i);
+            } else {
+                hashMap.put(word, 1);
+            }
+        }
         return hashMap;
-
     }
 }
